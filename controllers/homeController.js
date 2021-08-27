03e6a90347f7d959e561531a4b61920609ca5750
RadostinKirov <radopk@gmail.com>
2021-08-27 19:31:26 +0300

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,12 +4,18 @@ router.get('/', async (req, res) => {
     console.log('/ requested')
 
     let photos = await req.storage.getAllPhotos();
-    photos.sort((b, a) => {
+    
+    photos = photos.sort((b, a) => {
         let dateA = a.createdAt.slice(3);
         let dateB = b.createdAt.slice(3);
+        console.log('-> ',dateA);
         return dateA.localeCompare(dateB)
     }
     );
+
+    
+  
+
     res.status(200).send(photos);
 });
 
